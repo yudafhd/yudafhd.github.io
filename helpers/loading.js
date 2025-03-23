a3d0ca82268f089ab65e75342ebeedc3a3c316c1
Ahmad Yuda Fahrudin <yuda@Ahmads-MacBook-Pro.local>
2025-03-23 15:48:26 +0800

    init tailwind

---
+++ helpers/loading.js
@@ -0,0 +1,9 @@
+const loadingPage = document.getElementById("header-menu").classList;
+
+loadingPage.add("hidden");
+
+setTimeout(() => {
+  document.getElementById("loading-page").remove();
+  loadingPage.remove("hidden");
+  loadingPage.add("slide-down");
+}, 2000);
